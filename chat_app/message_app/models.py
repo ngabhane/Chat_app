647bf88c5b09c121f54a14255e6aeee35abe73aa
Nikhil Gabhane <nikhilgabhane29@gmail.com>
2016-10-21 13:53:55 +0530

    chat

---
+++ chat_app/message_app/models.py
@@ -0,0 +1,13 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+from django.contrib.auth.models import User
+
+class Chat(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    user 	= models.ForeignKey(User)
+    message = models.CharField(max_length=200)
+
+    def __unicode__(self):
+        return self.message
---
+++ v/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/nikhil/Documents/Tech/django_project/new_chat_app/v/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
